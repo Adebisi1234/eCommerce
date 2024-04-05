2453dfd5bdc273e180ff316689cd9b38d6914dea
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2024-04-06 00:18:16 +0100

    fix: working around render's constraints

--- python/start.sh
+++ python/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-temporal server start-dev & gunicorn 'Backend.app:create_app()' & python3 ./Backend/application/temporal/run_worker.py
+temporal server start-dev & gunicorn --timeout 0 'Backend.app:create_app()' & python3 ./Backend/application/temporal/run_worker.py
